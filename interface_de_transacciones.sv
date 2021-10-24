04787ba8340918bfc01ccf7f937c3e10f2a50a75
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-10-24 00:46:15 -0600

    Version casi final

--- interface_de_transacciones.sv
+++ interface_de_transacciones.sv
@@ -2,7 +2,7 @@
 //VERIFICACIÓN FUNCIONAL DE CIRCUITOS INTEGRADOS
 //Proyecto 2
 //Lenguaje: SystemVerilog
-//Creado por: Mac Alfred Pinnock Chacón (mcalfred32@gmail.com)
+//Creado por: Mac Alfred Pinnock Chacón (mcalfred32@gmail.com) - Susana Astorga Rodríguez (susana.0297.ar@gmail.com)
 
 //**************DEFINICION DE TIPOS DE TRANSACCION**************************
 typedef enum {ordenado, esquina, aleatorio, overflow} tipos_trans;//definir los casos de generación del test

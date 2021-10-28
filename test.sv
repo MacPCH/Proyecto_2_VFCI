ce883297e6322c2ad41e042771e469ee3bb8ed3a
Susana Astorga <43688242+astorga02@users.noreply.github.com>
2021-10-27 22:57:46 -0600

    versión final comentada

--- test.sv
+++ test.sv
@@ -2,24 +2,27 @@
 //VERIFICACIÓN FUNCIONAL DE CIRCUITOS INTEGRADOS
 //Proyecto 2
 //Lenguaje: SystemVerilog
-//Creado por: Mac Alfred Pinnock Chacón (mcalfred32@gmail.com)
+//Creado por: Mac Alfred Pinnock Chacón (mcalfred32@gmail.com) - Susana Astorga Rodríguez (susana.0297.ar@gmail.com)
 
 
-`include "Ambiente.sv"
+`include "ambiente.sv"
 class test#(parameter pckg_sz,FIFO_D,ROWS,COLUMS);
   int contador = 1;
   Ambiente #(.pckg_sz(pckg_sz),.esquina(esquina),.ROWS(ROWS),.COLUMS(COLUMS),.FIFO_D(FIFO_D)) ambiente_instancia; //se crea el ambiente
   
-  comando_test_generador_mbx test_generador_mbx;
+  comando_test_generador_mbx test_generador_mbx; //mailbox del test al generador
   tipos_de_transacciones instruccion_especifica;
-  comando_test_checker_mbx test_checker_mbx;
+  comando_test_checker_mbx test_checker_mbx; //mailbox del checker al test
   tipos_de_reportes tipo_reporte;
   event checker_listo;
   
   function new();
+    // Instanciación de los mailboxes
     test_generador_mbx = new();
     test_checker_mbx = new();
+    // instanciación de los componentes del test
     ambiente_instancia = new;
+    // conexión de las interfaces y mailboxes en el test
     ambiente_instancia.test_generador_mbx = test_generador_mbx;
     ambiente_instancia.generador_instancia.test_generador_mbx = test_generador_mbx;
     ambiente_instancia.test_checker_mbx = test_checker_mbx;
@@ -30,7 +33,7 @@ class test#(parameter pckg_sz,FIFO_D,ROWS,COLUMS);
   
   
   
-  task run();
+  task run(); //task donde correrá el test
     fork
     	ambiente_instancia.run();
     join_none
@@ -42,19 +45,19 @@ class test#(parameter pckg_sz,FIFO_D,ROWS,COLUMS);
     $display ("\\-- \\-- \\-- \\-- \\-- \\-- \\-- \\-- \\-- \\--");
    
     instruccion_especifica = new;
-  	instruccion_especifica.tipo = aleatorio;
-    instruccion_especifica.num_transacciones = 40;
+  	instruccion_especifica.tipo = aleatorio; //selección del tipo de transacción
+    instruccion_especifica.num_transacciones = 40; //número de transacciones
     $display ("Test: Enviado al generador: ", instruccion_especifica);
-    test_generador_mbx.put(instruccion_especifica);
+    test_generador_mbx.put(instruccion_especifica); //envío la transacccion con la instrucción específica hacia el test generador
     
-    //num_reportes: 1=ancho_banda, 2=retraso_total, 3=retraso_dispositivo, 4=todos, 5=ancho_banda y retraso_total 
-    //num_reportes: 6=ancho_banda y retraso_dispositivo, 7=retraso_total y retraso_dispositivo
+    //num_reportes: 1=ab, 2=retraso_total, 3=retraso_dispositivo, 4=todos, 5=ab y retraso_total 
+    //num_reportes: 6=ab y retraso_dispositivo, 7=retraso_total y retraso_dispositivo
     tipo_reporte = new();
     tipo_reporte.num_transacciones = instruccion_especifica.num_transacciones;
-    tipo_reporte.num_reportes = 1;
+    tipo_reporte.num_reportes = 1; //reporte del tipo ancho de banda
     tipo_reporte.profundidad_fifo = FIFO_D;
     tipo_reporte.fin_prueba = 100000 * contador;
-    test_checker_mbx.put(tipo_reporte);
+    test_checker_mbx.put(tipo_reporte); //envío el reporte generado hacia el test checker
     $display ("Test: Enviado al checker: ", tipo_reporte);
     
     
@@ -123,7 +126,7 @@ class test#(parameter pckg_sz,FIFO_D,ROWS,COLUMS);
     test_checker_mbx.put(tipo_reporte);
     
     #100000
-    ->checker_listo;
+    ->checker_listo; //se activa el evento
     #5
     $display ("-- \\ -- \\ -- \\ -- \\ -- \\ -- \\ -- \\ -- \\");
     $display ("-- / -- / -- / -- / -- / -- / -- / -- /");
